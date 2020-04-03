b339a5277c3ae0619e8742c0bdb44fccd914a439
cmoncadov <c.moncado@outlook.com>
2020-04-02 22:29:05 -0300

    Ultimas config docker + componentes vue y otras cosas

---
+++ resources/js/components/ThoughtsComponent.vue
@@ -0,0 +1,21 @@
+<template>
+<div class="panel panel-default">
+    <div class="panel-heading">Publicado en 17/17/18</div>
+
+    <div class="panel-body">
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+    </div>
+    <div class="panel-footer">
+      <button class="btn btn-default">Editar</button>
+      <button class="btn btn-danger">Eliminar</button>
+    </div>
+</div>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
---
+++ resources/js/components/formComponent.vue
@@ -0,0 +1,31 @@
+<template>
+<div class="panel panel-default">
+    <div class="panel-heading">¿En que estas pensando?</div>
+
+    <div class="panel-body">
+
+
+
+            </div>
+
+        <form  action="">
+          <div class="form-group">
+            <label for=""> Ahora pienso en: </label>
+            <input type="text" class="form-control" name="thoughts">
+
+          </div>
+          <button type="submit" class="btn btn-primary">
+            Enviar pensamiento
+          </button>
+        </form>
+    </div>
+
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
